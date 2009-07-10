4c4194b4ff8ef757e5f3239845f7915e65f84691
Ben Shymkiw <ben@t.apio.ca>
2009-07-10 15:41:16 -0400

    added logging

--- deploy.rb
+++ deploy.rb
@@ -8,9 +8,17 @@ post '/:repo_name' do
 
   repo_name = params[:repo_name]
   if repo = CONFIG['repos'][repo_name]
+    log = File.open(File.join(repo['path'], 'log', 'deploy.log'), 'a')
+    log.write '**************************************'
+    log.write "Starting deployment at #{Time.now}"
     pulled = system("cd #{repo['path']} && git pull #{repo['remote']} #{repo['branch']}")
+    log.write "Pulled: #{pulled}"
     migrated = system("cd #{repo['path']} && rake db:migrate RAILS_ENV=#{repo['env']}")
+    log.write "Migrated: #{migrated}"
     restarted = system("cd #{repo['path']} && repo['restart_command']")
+    log.write "Restarted: #{restarted}"
+    log.write "Finished."
+    log.close
   else
     throw :halt, [404, "<h1>Repo not found: #{repo_name}</h1>"]
   end
