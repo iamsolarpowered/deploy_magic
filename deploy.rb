b61f961003090d419ca033db9653e40763681598
Ben Shymkiw <ben@t.apio.ca>
2009-07-10 14:54:55 -0400

    run commands seperately

--- deploy.rb
+++ deploy.rb
@@ -1,19 +1,18 @@
 CONFIG = YAML.load_file('config.yml')
 
 post '/:repo_name' do
-  # Only allow requests from Github
+  # Only allow requests from Github's IP (specified in config.yml)
   unless @request.env['REMOTE_ADDR'] == CONFIG['github_ip']
     throw :halt, [401, '<h1>Permission denied.</h1>']
   end
 
   repo_name = params[:repo_name]
   if repo = CONFIG['repos'][repo_name]
-    cmd = ["cd #{repo['path']}"]
-    cmd << "git pull #{repo['remote']} #{repo['branch']}"
-    cmd << "rake db:migrate RAILS_ENV=#{repo['env']}"
-    cmd << repo['restart_command']
-    system(cmd.join(' && '))
-    '<h1>Deployed :)</h1>'
+    ok = true
+    ok = false unless system("#{repo['path']}/git pull #{repo['remote']} #{repo['branch']}")
+    ok = false unless system("#{repo['path']}/rake db:migrate RAILS_ENV=#{repo['env']}")
+    ok = false unless system("#{repo['path']}/repo['restart_command']")
+    (ok ? '<h1>Deployed :)</h1>' : '<h1>Something went wrong :(')
   else
     throw :halt, [404, "<h1>Repo not found: #{repo_name}</h1>"]
   end
