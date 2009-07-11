eea8d9430fadbb0657995af5adb8a4de9380373a
Ben Shymkiw <ben@t.apio.ca>
2009-07-11 03:02:50 -0400

    switched to HTTP auth

--- deploy.rb
+++ deploy.rb
@@ -1,25 +1,25 @@
+require 'erb'
+require 'builder'
+
 CONFIG = YAML.load_file('config.yml')
 
-post '/:repo_name' do
-  # Only allow requests from Github's IP (specified in config.yml)
-  unless @request.env['REMOTE_ADDR'] == CONFIG['github_ip']
-    throw :halt, [401, '<h1>Permission denied.</h1>']
-  end
 
+use Rack::Auth::Basic do |username, password|
+  [username, password] == [CONFIG['username'], CONFIG['password']]
+end
+
+post '/:repo_name' do
   repo_name = params[:repo_name]
   if repo = CONFIG['repos'][repo_name]
-    log = File.open(File.join(repo['path'], 'log', 'deploy.log'), 'a')
-    log.write '**************************************'
-    log.write "Starting deployment at #{Time.now}"
-    pulled = system("cd #{repo['path']} && git pull #{repo['remote']} #{repo['branch']}")
-    log.write "Pulled: #{pulled}"
-    migrated = system("cd #{repo['path']} && rake db:migrate RAILS_ENV=#{repo['env']}")
-    log.write "Migrated: #{migrated}"
-    restarted = system("cd #{repo['path']} && repo['restart_command']")
-    log.write "Restarted: #{restarted}"
-    log.write "Finished."
-    log.close
+    @pulled = system("cd #{repo['path']} && git pull #{repo['remote']} #{repo['branch']}")
+    @migrated = system("cd #{repo['path']} && rake db:migrate RAILS_ENV=#{repo['env']}")
+    @restarted = system("cd #{repo['path']} && repo['restart_command']")
+    builder :response
   else
     throw :halt, [404, "<h1>Repo not found: #{repo_name}</h1>"]
   end
+end
+
+get '/' do
+  erb :index
 end
---
+++ start_server.rb
@@ -0,0 +1,4 @@
+require 'rubygems'
+require 'vendor/rack/lib/rack'
+require 'vendor/sinatra/lib/sinatra.rb'
+require 'deploy'
